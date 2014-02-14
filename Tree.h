1c75bfe0e3c2fac4f2390a28bf01d0e9e2487e33
Stephen Ward <sjward@gmail.com>
2014-02-13 22:09:48 -0800

    Still adding stuff

--- Tree.h
+++ Tree.h
@@ -1,8 +1,7 @@
 #ifndef TREE_H
 #define TREE_H
 
-// TODO:
-// * iterators
+#include <memory>
 
 template <class T>
 class Tree
@@ -18,31 +17,101 @@ public:
     bool remove(const T& value);
     bool contains(const T& value) const;
 
-    size_t size() const;
+    size_t size() const { return mSize; }
+
+    // TODO: remove
+    void print() const
+    {
+        if (mRoot) {
+            print(mRoot);
+        }
+    }
+
+    class iterator
+    {
+        // TODO: ???
+    };
 
 private:
     Tree(const Tree& t) = default;
     Tree& operator=(const Tree& t) = default;
 
-    class Node
+    struct Node
     {
-    public:
-        Node* parent;
-        Node* left;
-        Node* right;
+        Node()
+        { }
+        Node(const T& v) : value(v)
+        { }
+
+        std::shared_ptr<Node> parent;
+        std::shared_ptr<Node> left;
+        std::shared_ptr<Node> right;
         T value;
     };
+    std::shared_ptr<Node> mRoot;
+    size_t mSize;
+
+    // TODO: remove
+    void print(const std::shared_ptr<Node>& node) const
+    {
+        if (node->left) {
+            print(node->left);
+        }
+        std::cout << node->value << std::endl;
+        if (node->right) {
+            print(node->right);
+        }
+    }
 };
 
 
 template <class T>
 Tree<T>::Tree()
+    : mSize(0)
 {
 }
 
 template <class T>
 Tree<T>::~Tree()
 {
+}
+
+template <class T>
+bool
+Tree<T>::add(const T& value)
+{
+    if (!mRoot) {
+        mRoot.reset(new Node(value));
+        mSize++;
+        return true;
+    }
+
+    std::shared_ptr<Node>& current = mRoot;
+    while (true) {
+        if (value == current->value) {
+            // Already in the tree.
+            return false;
+        }
+
+        if (value < current->value) {
+            if (!current->left) {
+                current->left.reset(new Node(value));
+                current->left->parent = current;
+                mSize++;
+                return true;
+            }
+            current = current->left;
+        } else {
+            if (!current->right) {
+                current->right.reset(new Node(value));
+                current->right->parent = current;
+                mSize++;
+                return true;
+            }
+            current = current->right;
+        }
+    }
+    return false;
 }
 
 #endif // TREE_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
+
 #include "Tree.h"
 
 int main(int argc, char* argv[])
 {
+    std::srand(std::time(0));
+
     Tree<int> t;
+
+    std::vector<int> randomData = { 1, 28, 81, /*51, 53, 6, 51, 42, 87, 34,*/ };
+    for (int x : randomData) {
+        t.add(x);
+    }
+
+    //for (int i = 0; i < 10; ++i) {
+    //    int value = std::rand() % 100;
+    //    std::cout << value << std::endl;
+    //    t.add(value);
+    //}
+    //std::cout << std::endl;
+    t.print();
+    std::cout << "size: " << t.size() << std::endl;
+
     return 0;
 }
